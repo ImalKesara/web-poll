1bbd7569428b605726b21841e8869eaebc20558c
LoneWolf <imalkesara2001@gmail.com>
2024-06-13 02:50:58 +0530

    setup footer component

---
+++ src/Components/Footer/Footer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts"></script>
+
+<footer>
+    <div class=" bg-orange-600 text-white p-4 font-bold">
+        Developed by <span class="text-black">Dark Wolf </span>
+    </div>
+</footer>
