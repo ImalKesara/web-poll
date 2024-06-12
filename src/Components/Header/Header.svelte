e89fe8b19214628ba1ffb0bf94a231874abc37cc
LoneWolf <imalkesara2001@gmail.com>
2024-06-13 02:46:41 +0530

    logo and content

---
+++ src/Components/Header/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<header>
+    <h1 class="flex  justify-center bg-slate-300 m-0">
+        <img class="h-24" src="src/img/front.png" alt="Frontend image">
+    </h1>
+</header>
+
+<style>
+    
+</style>
