0dc09e4b9a061db214b150a6dc01a74e19ba9507
LoneWolf <imalkesara2001@gmail.com>
2024-06-13 02:46:27 +0530

    import header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+  import Header from './Components/Header/Header.svelte';
   import './app.css';
 </script>
 
 
+<Header />
+<main>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem architecto maxime nulla esse cumque dolores itaque voluptate consequuntur asperiores odit, pariatur qui temporibus minus adipisci? Sit incidunt sunt ad itaque!</p>
+</main>
+
+
+<style>
+  main{
+    max-width: 960px;
+    margin: 1em auto;
+  }
+
+</style>
