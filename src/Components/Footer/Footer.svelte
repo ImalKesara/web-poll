93e09217544b06a96b903dadb1ff75d663e206a9
LoneWolf <imalkesara2001@gmail.com>
2024-06-13 13:41:54 +0530

    center

--- src/Components/Footer/Footer.svelte
+++ src/Components/Footer/Footer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts"></script>
 
-<footer>
-    <div class=" bg-orange-600 text-white p-4 font-bold">
+<footer class="text-center p-4">
+    <div class="border-y-2 border-sky-400 p-3  font-bold inline-block">
         Developed by <span class="text-black">Dark Wolf </span>
     </div>
 </footer>
