36f2e487917221481d4410d37f9e7ad04021fe00
LoneWolf <imalkesara2001@gmail.com>
2024-06-13 14:13:28 +0530

    dispatch for tab change

--- src/Shared/Tabs.svelte
+++ src/Shared/Tabs.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
     export let items : string[];
     export let activeItem :string;
 </script>
@@ -7,8 +10,8 @@
 <div class="tabs">
     <ul class="flex justify-center p-0 ">
     {#each items as item}
-        <li class="mx-5 text-2xl text-gray-400 cursor-pointer">
-            <div class="inline-block" class:active ={item===activeItem} >{item}</div>
+        <li class="mx-5 text-xl text-gray-400 cursor-pointer" on:click={()=>dispatch('tabChange',item)} >
+            <div class:active ={item===activeItem} >{item}</div>
         </li>
     {/each}
     </ul>
@@ -20,6 +23,6 @@
     }
 
     .active{
-        @apply text-red-500 pe-10 border-b-2 border-red-300
+        @apply text-red-500  border-b-2 border-red-500
     }
 </style>
