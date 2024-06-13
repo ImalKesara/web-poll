df487bd7c3eccee236f1f172f583339cfad713cc
LoneWolf <imalkesara2001@gmail.com>
2024-06-13 15:04:20 +0530

    created onject to store form information and tab (add new polls) moslty done

--- src/Components/CreatePollForm/CreatePollForm.svelte
+++ src/Components/CreatePollForm/CreatePollForm.svelte
@@ -9,24 +9,32 @@
     }
 </script>
 
-<form on:submit|preventDefault ={submitHandle} >
+<form on:submit|preventDefault ={submitHandle} class="w-4/5 m-auto">
         <div class="form-field">
-            <label for="question">Poll Question</label>
-            <input id ="question" type="text" placeholder="ko hutto" bind:value={fields.Question}>
+            <label for="question">Poll Question :</label>
+            <input id ="question" type="text" placeholder="Question" bind:value={fields.Question}>
         </div>
         <div class="form-field">
-            <label for="answer-a">Answer A</label>
-            <input id ="answer-a" type="text" placeholder="ko hutto" bind:value={fields.answerA}>
+            <label for="answer-a">Answer A :</label>
+            <input id ="answer-a" type="text" placeholder="Answer A" bind:value={fields.answerA}>
         </div>
         <div class="form-field">
-            <label for="answer-b">Answer B</label>
-            <input id ="answer-b" type="text" placeholder="ko hutto" bind:value={fields.answerB}>
+            <label for="answer-b">Answer B :</label>
+            <input id ="answer-b" type="text" placeholder="Answer B" bind:value={fields.answerB}>
         </div>
-   <button>Add Poll</button>
+        <div class="flex justify-center">
+            <button class="bg-sky-500 p-3 rounded-md text-white font-bold ease duration-300 hover:bg-sky-600 inline-block">Add Poll</button>
+        </div>
 </form>
 
 <style>
     .form-field{
-        @apply flex flex-col
+        @apply flex flex-col my-8
+    }
+    label{
+        @apply py-3
+    }
+    input{
+        @apply h-10 border-2 p-2 rounded-md
     }
 </style>
