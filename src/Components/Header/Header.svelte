18a4280d9be8f89ffe510ded38e92a75b3511743
LoneWolf <imalkesara2001@gmail.com>
2024-06-13 16:01:14 +0530

    font - mono

--- src/Components/Header/Header.svelte
+++ src/Components/Header/Header.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <header>
-    <h1 class="flex  justify-center bg-sky-300 m-0">
-        <img class="h-24" src="src/img/front.png" alt="Frontend image">
+    <h1 class="flex font-mono justify-center bg-sky-300 m-0 text-3xl p-4 text-white font-bold">
+        Question Poll
     </h1>
 </header>
 
