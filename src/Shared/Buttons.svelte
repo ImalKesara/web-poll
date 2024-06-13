4aa68af122961aa6499f4aac5cf8637cba236d24
LoneWolf <imalkesara2001@gmail.com>
2024-06-13 15:46:43 +0530

    set shadows

--- src/Shared/Buttons.svelte
+++ src/Shared/Buttons.svelte
@@ -2,10 +2,10 @@
     export let colors = "sky";
 
     const COLORS : string ={
-        "sky" : "bg-sky-500 hover:bg-sky-600 p-3 rounded-md text-white font-bold ease duration-300",
-        "red" : "bg-red-500 hover:bg-red-600 p-3 rounded-md text-white font-bold ease duration-300",
-        "purple" : "bg-purple-500 hover:bg-purple-600 p-3 rounded-md text-white font-bold ease duration-300",
-        "orange" : "bg-orange-500 hover:bg-orange-600 p-3 rounded-md text-white font-bold ease duration-300",
+        "sky" : "bg-sky-500 hover:bg-sky-600 p-3 rounded-md text-white font-bold ease duration-300 drop-shadow-lg",
+        "red" : "bg-red-500 hover:bg-red-600 p-3 rounded-md text-white font-bold ease duration-300 drop-shadow-lg",
+        "purple" : "bg-purple-500 hover:bg-purple-600 p-3 rounded-md text-white font-bold ease duration-300 drop-shadow-lg",
+        "orange" : "bg-orange-500 hover:bg-orange-600 p-3 rounded-md text-white font-bold ease duration-300 drop-shadow-lg",
     } 
 
     const getColor  = (color)=>{
