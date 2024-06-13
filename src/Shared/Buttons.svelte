5b4318ede66985f3c1a1d313ea137e51ea22a255
LoneWolf <imalkesara2001@gmail.com>
2024-06-13 15:44:32 +0530

    custom button component

---
+++ src/Shared/Buttons.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let colors = "sky";
+
+    const COLORS : string ={
+        "sky" : "bg-sky-500 hover:bg-sky-600 p-3 rounded-md text-white font-bold ease duration-300",
+        "red" : "bg-red-500 hover:bg-red-600 p-3 rounded-md text-white font-bold ease duration-300",
+        "purple" : "bg-purple-500 hover:bg-purple-600 p-3 rounded-md text-white font-bold ease duration-300",
+        "orange" : "bg-orange-500 hover:bg-orange-600 p-3 rounded-md text-white font-bold ease duration-300",
+    } 
+
+    const getColor  = (color)=>{
+        return COLORS[color] ?? COLORS["sky"];
+    }
+
+</script>
+
+<button>
+    <button  class="{getColor(colors)}"><slot></slot></button>
+</button>
+
+<style>
+
+</style>
