25b369430d2d54a1706d9f2ea4082071ccca1fa1
LoneWolf <imalkesara2001@gmail.com>
2024-06-16 01:03:20 +0530

    poll list componet to display in current poll

---
+++ src/Components/PollList/Polllist.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import PollDetails from '../PollDetails/PollDetails.svelte';
+
+  export let polls = []; //default value was empty
+</script>
+
+<div class="poll-list">
+  {#each polls as poll (poll.id)}
+    <div class="list">
+      <PollDetails {poll} />
+    </div>
+  {/each}
+</div>
+
+<style>
+  .poll-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 2em;
+  }
+</style>
