458122c9660a0f6c62dac901e36f21a9b0a5e694
LoneWolf <imalkesara2001@gmail.com>
2024-06-16 01:38:54 +0530

    styling

--- src/Components/PollDetails/PollDetails.svelte
+++ src/Components/PollDetails/PollDetails.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   export let poll;
+  $: totalVotes = poll.voteA + poll.voteB;
 </script>
 
 <div class="poll font-md capitalize">
-  <h1 class=" text-2xl font-bold">{poll.Question}</h1>
-  <div class="answers">
+  <h1 class=" text-2xl font-bold text-gray-500">{poll.Question}</h1>
+  <p class=" mt-2 text-sm mb-7">Total votes : {totalVotes}</p>
+  <div
+    class="answers cursor-pointer my-3 mx-auto relative hover:opacity-50 ease-in duration-200"
+  >
     <div class="percent percent-a"></div>
     <span>{poll.answerA} ({poll.voteA})</span>
 
@@ -14,4 +18,7 @@
 </div>
 
 <style>
+  span {
+    @apply inline-block py-2 px-5 bg-gray-200 w-full m-1 rounded-lg font-bold;
+  }
 </style>
