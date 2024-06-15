4f5c02e559ec7707d45e20a85219c392bad68e57
LoneWolf <imalkesara2001@gmail.com>
2024-06-16 00:32:25 +0530

    store function to store polls

--- src/App.svelte
+++ src/App.svelte
@@ -2,39 +2,55 @@
   import CreatePollForm from './Components/CreatePollForm/CreatePollForm.svelte';
   import Footer from './Components/Footer/Footer.svelte';
   import Header from './Components/Header/Header.svelte';
+  import Polllist from './Components/PollList/Polllist.svelte';
   import Tabs from './Shared/Tabs.svelte';
   import './app.css';
 
-  let items = ['Current Polls' ,'Add New Polls'];
+  let items = ['Current Polls', 'Add New Polls'];
   let activeItem = 'Current Polls';
 
-  function tabChange(e){
-    activeItem = e.detail
-    console.log('tag changed')
+  function tabChange(e) {
+    activeItem = e.detail;
+    console.log('tag changed');
   }
 
+  //polls
+  let polls = [
+    {
+      id: 1,
+      Question: 'Javacript or python',
+      answerA: 'javascript',
+      answerB: 'python',
+      voteA: 9,
+      voteB: 10,
+    },
+  ];
+
+  function store(e) {
+    const poll = e.detail;
+    polls = [...polls, poll];
+    console.log(polls);
+    activeItem = 'Current Polls';
+  }
 </script>
-
-
-
 
 <Header />
 <main>
   <!-- you case use shorthand {items} like this -->
-  <Tabs items= {items} activeItem = {activeItem}  on:tabChange={tabChange} />
-  {#if activeItem === "Current Polls"}
-    <p>Current poll here</p>
-  {:else if activeItem === "Add New Polls"}
-    <CreatePollForm />
+  <Tabs {items} {activeItem} on:tabChange={tabChange} />
+  {#if activeItem === 'Current Polls'}
+    {#each polls as poll}
+      <Polllist />
+    {/each}
+  {:else if activeItem === 'Add New Polls'}
+    <CreatePollForm on:store={store} />
   {/if}
 </main>
 <Footer />
 
-
 <style>
-  main{
+  main {
     max-width: 960px;
     margin: 1em auto;
   }
-
 </style>
