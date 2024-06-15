8d04dc862bf7c56fa172fada0a5dcd8f5a566405
LoneWolf <imalkesara2001@gmail.com>
2024-06-16 01:04:07 +0530

    all information that polls array

---
+++ src/Components/PollDetails/PollDetails.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let poll;
+</script>
+
+<div class="poll font-md capitalize">
+  <h1 class=" text-2xl font-bold">{poll.Question}</h1>
+  <div class="answers">
+    <div class="percent percent-a"></div>
+    <span>{poll.answerA} ({poll.voteA})</span>
+
+    <div class="percent percent-b"></div>
+    <span>{poll.answerB} ({poll.voteB})</span>
+  </div>
+</div>
+
+<style>
+</style>
