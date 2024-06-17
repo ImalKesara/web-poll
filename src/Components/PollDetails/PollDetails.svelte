74c458431b25e7e64f190a870d83e0d66a272e57
LoneWolf <imalkesara2001@gmail.com>
2024-06-17 18:17:11 +0530

    dynmically updating vote bar

--- src/Components/PollDetails/PollDetails.svelte
+++ src/Components/PollDetails/PollDetails.svelte
@@ -10,6 +10,8 @@
   }
 
   $: totalVotes = poll.voteA + poll.voteB;
+  $: percentageA = Math.floor((poll.voteA / totalVotes) * 100);
+  $: percentageB = Math.floor((poll.voteB / totalVotes) * 100);
 </script>
 
 <Card>
@@ -20,7 +22,7 @@
       class="answers cursor-pointer hover:opacity-50 ease-in duration-200 flex flex-col gap-1"
       on:click={() => handleVote('a', poll.id)}
     >
-      <div class="percent percent-a"></div>
+      <div class="percent percent-a" style="width: {percentageA}%"></div>
       <span>{poll.answerA} ({poll.voteA})</span>
     </div>
 
@@ -28,7 +30,7 @@
       class="answers cursor-pointer hover:opacity-50 ease-in duration-200 flex flex-col gap-1"
       on:click={() => handleVote('b', poll.id)}
     >
-      <div class="percent percent-b"></div>
+      <div class="percent percent-b" style="width: {percentageB}%;"></div>
       <span>{poll.answerB} ({poll.voteB})</span>
     </div>
   </div>
@@ -37,5 +39,24 @@
 <style>
   span {
     @apply inline-block py-3 px-5 bg-gray-200 w-full rounded-lg font-bold;
+  }
+
+  .answers {
+    @apply relative mb-1;
+  }
+
+  .percent {
+    height: 100%;
+    @apply absolute box-border flex flex-col;
+  }
+
+  .percent-a {
+    border-left: 3px solid green;
+    @apply opacity-50 rounded-r-lg bg-green-300 ease-in duration-150;
+  }
+
+  .percent-b {
+    border-left: 3px solid red;
+    @apply opacity-50 rounded-r-lg bg-red-300 ease-in duration-150;
   }
 </style>
